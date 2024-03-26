618b07b08ea209703eb006806977091cd881ebfe
stich2005 <orlan2005@gmil.com>
2024-03-26 13:27:26 +1000

    no message

--- LifeBook/LifeBook/settings.py
+++ LifeBook/LifeBook/settings.py
@@ -74,23 +74,23 @@ WSGI_APPLICATION = 'LifeBook.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-#DATABASES = {
- #   'default': {
-       # 'ENGINE': 'django.db.backends.mysql',
-      #  'NAME': 'if0_36198848_01',
-     #   'USER': 'if0_36198848',
-    #    'PASSWORD': 'NoL6V2ul28',
-   #     'HOST': 'sql210.infinityfree.com',
-  #      'PORT': '3306'
- #   }
-#}
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+   'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'if0_36198848_01',
+        'USER': 'if0_36198848',
+        'PASSWORD': 'NoL6V2ul28',
+        'HOST': 'sql210.infinityfree.com',
+        'PORT': '3306'
     }
 }
+
+#DATABASES = {
+#    'default': {
+ #       'ENGINE': 'django.db.backends.sqlite3',
+ #       'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
